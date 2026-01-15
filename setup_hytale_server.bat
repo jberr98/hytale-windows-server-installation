c4d62695b37ac9321a00aa5418e982b92bd2e18a
Javier Berrezueta <33329664+jberr98@users.noreply.github.com>
2026-01-14 23:21:13 -0500

    Fixing incomplete backup parameter and added script for updating server
    Backup parameter needed dir-path, when not provided, it crashed the server.

--- setup_hytale_server.bat
+++ setup_hytale_server.bat
@@ -21,11 +21,36 @@ del hytale-server.zip
 del hytale-downloader-linux-amd64
 
 :: Just outputs the hytale downloader version, not necessary
-.\hytale-downloader-windows-amd64.exe -version > version.txt
+.\hytale-downloader-windows-amd64.exe -version > downloader-version.txt
+:: Just outputs the hytale server version, not necessary
+.\hytale-downloader-windows-amd64.exe -print-version > game-version.txt
 
-:: Creates bat file that starts the server with: Caching to boot the server faster, minimum amount of memory 128Mb, authenticated server and makes the server use the port 5520
-echo java -XX:AOTCache=Server\HytaleServer.aot -Xms128M -jar Server/HytaleServer.jar --assets Assets.zip --auth-mode authenticated --backup --bind 0.0.0.0:5520 > start_server.bat
+mkdir Backups
+
+:: Creates bat file that starts the server with: Caching to boot the server faster, minimum amount of memory 4G, authenticated server and makes the server use the port 5520
+echo java -XX:AOTCache=Server\HytaleServer.aot -Xms4G -jar Server/HytaleServer.jar --assets Assets.zip --auth-mode authenticated --backup --backup-dir .\Backups --bind 0.0.0.0:5520 > start_server.bat
+
+:: Creates bat script to update the server
+(
+echo @echo OFF
+echo echo Starting update process
+echo .\hytale-downloader-windows-amd64.exe -download-path hytale-server.zip
+echo tar -xf hytale-server.zip
+echo del hytale-server.zip
+echo.
+echo :: Just outputs the hytale downloader version, not necessary
+echo .\hytale-downloader-windows-amd64.exe -version ^> downloader-version.txt
+echo :: Just outputs the hytale server version, not necessary
+echo .\hytale-downloader-windows-amd64.exe -print-version ^> game-version.txt
+echo.
+echo echo Update finished
+echo pause
+) > update_server.bat
+
+:: Prevents people from unknowingly uploading their credentials by accident, you can ignore or delete the file if you are not planning on uploading the server, but recommend leaving it just in case
+echo .hytale-downloader-credentials.json > .gitignore
 
 echo Done! Server ready
 
 pause
+
