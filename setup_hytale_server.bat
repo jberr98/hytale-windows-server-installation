c1b7fd98b49233080e815a7440a6dbbc87eb438e
Javier Berrezueta <33329664+jberr98@users.noreply.github.com>
2026-01-14 21:59:37 -0500

    Added a few more comments to make it clearer

--- setup_hytale_server.bat
+++ setup_hytale_server.bat
@@ -2,7 +2,7 @@
 :: Hytale Server bat script for Windows server
 :: It relies on an empty folder. Gets the basics to setup a Hytale server.
 
-set DOWNLOAD_URL=https://downloader.hytale.com/hytale-downloader.zip    
+set DOWNLOAD_URL=https://downloader.hytale.com/hytale-downloader.zip
 
 echo Downloading Hytale downloader
 curl -O %DOWNLOAD_URL%
@@ -20,10 +20,12 @@ del hytale-server.zip
 :: Script used for Windows. Doesn't need Linux binary in this case.
 del hytale-downloader-linux-amd64
 
-:: Just outputs the server version, not necessary
+:: Just outputs the hytale downloader version, not necessary
 .\hytale-downloader-windows-amd64.exe -version > version.txt
 
+:: Creates bat file that starts the server with: Caching to boot the server faster, minimum amount of memory 128Mb, authenticated server and makes the server use the port 5520
 echo java -XX:AOTCache=Server\HytaleServer.aot -Xms128M -jar Server/HytaleServer.jar --assets Assets.zip --auth-mode authenticated --backup --bind 0.0.0.0:5520 > start_server.bat
 
 echo Done! Server ready
+
 pause
